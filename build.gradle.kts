00f2980f9cc8abed8137ac46e247630b18e9f300
Iulia STANA <iulia@egeniq.com>
2023-11-20 10:56:54 +0100

    TIQR-334 Update java version for github actions Update core to better work with ksp Integrate new core, add scripts for testing backup & restore Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,27 +1,14 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-
-    dependencies {
-        classpath(libs.android.gradle)
-        classpath(libs.kotlin.gradle)
-        classpath(libs.dagger.hilt.gradle)
-        classpath(libs.androidx.navigation.gradle)
-        classpath(libs.google.gms.gradle)
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
-    }
-}
-
-subprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.safe.args) apply false
+    //Try to avoid using KAPT, check if the library can work/supports KSP which is much faster.
+    alias(libs.plugins.kapt) apply false
+    //The plugin to use i.s.o. KAPT: https://developer.android.com/build/migrate-to-ksp
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.google.gms.gradle) apply false
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+
 rootProject.name = "tiqr app android"
 
 include(":app")
@@ -9,8 +25,3 @@ project(":core").projectDir = File("app-core/core")
 // Add data
 include(":data")
 project(":data").projectDir = File("app-core/data")
-
-
-// Enable Gradle's version catalog support
-// https://docs.gradle.org/current/userguide/platforms.html
-enableFeaturePreview("VERSION_CATALOGS")
